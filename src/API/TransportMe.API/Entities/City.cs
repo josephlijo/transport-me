f09710cb986f17453f543a3bf7dbb74dc2d46ffc
josephlijo <joseph_lijo@outlook.com>
2018-04-09 16:57:54 +0100

    Create EF entities
    Entities for City, Transport Mode and Service

---
+++ src/API/TransportMe.API/Entities/City.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TransportMe.API.Entities
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+    }
+}
