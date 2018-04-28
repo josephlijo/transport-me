27cb4aeea51e6175770bc648dd8628925edb905b
josephlijo <joseph_lijo@outlook.com>
2018-04-28 19:45:37 +0100

    Add EF and Unit test changes

--- Places/src/TransportMe.Places.API/Infrastructure/PlacesContextSeed.cs
+++ Places/src/TransportMe.Places.API/Infrastructure/PlacesContextSeed.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TransportMe.Places.API.Infrastructure
+﻿namespace TransportMe.Places.API.Infrastructure
 {
     // Todo:// read data from CSV files to and seed
     public class PlacesContextSeed
